28ebd58e8ce4f45334929a6cf9181b35ae7b7e32
Gary Kinkladze <110338310+GYRAG@users.noreply.github.com>
2023-06-30 23:39:36 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,97 @@
-import React, { useState, useReducer, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 
-const initialState = {
-  tasks: [],
-  completedTasks: [],
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'ADD_TASK':
-      return {
-        ...state,
-        tasks: [
-          ...state.tasks,
-          { id: Date.now(), name: action.payload },
-        ],
-      };
-    case 'COMPLETE_TASK':
-      const taskToComplete = state.tasks.find(
-        (task) => task.id === action.payload
-      );
-      return {
-        tasks: state.tasks.filter((task) => task.id !== action.payload),
-        completedTasks: [...state.completedTasks, taskToComplete],
-      };
-    case 'DELETE_TASK':
-      return {
-        tasks: state.tasks.filter((task) => task.id !== action.payload),
-        completedTasks: state.completedTasks.filter(
-          (task) => task.id !== action.payload
-        ),
-      };
-    case 'MOVE_TO_TODO':
-      const taskToMove = state.completedTasks.find(
-        (task) => task.id === action.payload
-      );
-      return {
-        tasks: [...state.tasks, taskToMove],
-        completedTasks: state.completedTasks.filter(
-          (task) => task.id !== action.payload
-        ),
-      };
-    default:
-      return state;
-  }
-}
-
 function TodoList() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
 
-  const handleNewTask = (e) => {
+  const handleNewTask = useCallback((e) => {
     e.preventDefault();
-    const taskName = e.target.elements.task.value;
+    const taskName = e.target.elements.task.value.trim();
     if (taskName) {
-      dispatch({ type: 'ADD_TASK', payload: taskName });
-      e.target.elements.task.value = '';
+      const newTask = {
+        id: Date.now(),
+        name: taskName,
+      };
+      setTasks((prevTasks) => [...prevTasks, newTask]);
+      e.target.reset();
     }
-  };
-
-  const handleTaskComplete = (taskId) => {
-    dispatch({ type: 'COMPLETE_TASK', payload: taskId });
-  };
-
-  const handleDeleteTask = (taskId, isCompleted) => {
-    dispatch({ type: 'DELETE_TASK', payload: taskId });
-  };
-
-  const handleMoveToTodo = (taskId) => {
-    dispatch({ type: 'MOVE_TO_TODO', payload: taskId });
-  };
-
-  useEffect(() => {
-    console.log('Component did mount');
-
-    return () => {
-      console.log('Component will unmount');
-    };
   }, []);
 
-  useEffect(() => {
-    console.log('Tasks changed:', state.tasks);
-  }, [state.tasks]);
+  const handleTaskComplete = useCallback((taskId) => {
+    const completedTask = tasks.find((task) => task.id === taskId);
+    setCompletedTasks((prevTasks) => [...prevTasks, completedTask]);
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  }, [tasks]);
 
-  useEffect(() => {
-    console.log('Completed tasks changed:', state.completedTasks);
-  }, [state.completedTasks]);
+  const handleDeleteTask = useCallback((taskId, isCompleted) => {
+    const updatedTasks = isCompleted
+      ? completedTasks.filter((task) => task.id !== taskId)
+      : tasks.filter((task) => task.id !== taskId);
+
+    if (isCompleted) {
+      setCompletedTasks(updatedTasks);
+    } else {
+      setTasks(updatedTasks);
+    }
+  }, [completedTasks, tasks]);
 
   return (
     <div>
       <h1>To-Do List აპლიკაცია</h1>
 
       <form onSubmit={handleNewTask}>
-        <input type="text" name="task" placeholder="შეიყვანეთ ახალი დავალება" />
-        <button type="submit">დავალების დამატება</button>
+        <input type="text" name="task" placeholder="Enter a new task" />
+        <button type="submit">დაამატე დავალებები</button>
       </form>
 
       <div>
-        <h2>დასასრულებელი დავალებები</h2>
+        <h2>შესასრულებელი დავალება</h2>
         <ul>
-          {state.tasks.map((task) => (
-            <li key={task.id}>
-              {task.name}
-              <button onClick={() => handleTaskComplete(task.id)}>
-                დასრულებულებში გადატანა
-              </button>
-              <button onClick={() => handleDeleteTask(task.id, false)}>
-                წაშლა
-              </button>
-            </li>
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onComplete={handleTaskComplete}
+              onDelete={handleDeleteTask}
+              isCompleted={false}
+            />
           ))}
         </ul>
       </div>
 
       <div>
-        <h2>დასრულებული დავალებები</h2>
+        <h2>შესრულებული დავალებები</h2>
         <ul>
-          {state.completedTasks.map((task) => (
-            <li key={task.id}>
-              {task.name}
-              <button onClick={() => handleDeleteTask(task.id, true)}>
-                წაშლა
-              </button>
-              <button onClick={() => handleMoveToTodo(task.id)}>
-                დაუსრულებლებში გადატანა
-              </button>
-            </li>
+          {completedTasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onDelete={handleDeleteTask}
+              isCompleted={true}
+            />
           ))}
         </ul>
       </div>
     </div>
+  );
+}
+
+function TaskItem({ task, onComplete, onDelete, isCompleted }) {
+  const handleComplete = useCallback(() => {
+    onComplete(task.id);
+  }, [onComplete, task.id]);
+
+  const handleDelete = useCallback(() => {
+    onDelete(task.id, isCompleted);
+  }, [onDelete, task.id, isCompleted]);
+
+  return (
+    <li>
+      {task.name}
+      <button onClick={handleComplete}>შესასრულებლებში გადატანა</button>
+      <button onClick={handleDelete}>წაშლა</button>
+    </li>
   );
 }
 
